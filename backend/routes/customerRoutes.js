11df1c5c32cabdc64fec848f72ce922a2d815d27
NandithaHari6 <117524858+NandithaHari6@users.noreply.github.com>
2023-11-22 11:22:20 +0530

    Update customerRoutes.js

--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -4,4 +4,5 @@ const customer=require("../controller/customerController");
 router.post("/addCustomer",customer.addNewCustomer);
 router.get("/getPersonalDetails:customerId",customer.getPersonalDetails);
 router.get("/getLoanDetails:customerId",customer.getLoanDetails);
+router.put("/update:customerId",customer.updateCustomerDetails);
 module.exports=router;
