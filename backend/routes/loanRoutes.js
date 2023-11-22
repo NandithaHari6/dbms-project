0ad2cefb9f67b150bd592f973aa41219be095b4a
NandithaHari6 <117524858+NandithaHari6@users.noreply.github.com>
2023-11-22 12:00:45 +0530

    Update loanRoutes.js

--- backend/routes/loanRoutes.js
+++ backend/routes/loanRoutes.js
@@ -5,6 +5,6 @@ const  loan= require('../controller/loanController');
 router.post("/addLoan",loan.addLoan);
 router.get("/getAllLoans:bankId",loan.getAllLoans);
 
-router.post("/delete:loanId",loan.deleteLoan);
+router.get("/delete:loanId",loan.deleteLoan);
 router.get("/getLoanDetails:customerId",customer.getLoanDetails);
 module.exports=router;
